160362b81225b4a815230b013ddc46f848289fa8
lenam19932012@gmail.com <lenam19932012@gmail.com>
2023-11-04 03:00:45 +0700

    update

--- schedule.js
+++ schedule.js
@@ -10,8 +10,12 @@ var files = [];
 var schedules = [];
 var pathSave = process.env.PATH_SAVE;
 var diffTime = process.env.DIFF_TIME;
+var maxRetry = process.env.MAX_RETRY;
 function deleteFolderOld(channel, date, keepDay) {
     var keepFolders = [];
+    if (!fs.existsSync(join(pathSave, 'videos', channel))) {
+        return;
+    }
     var dirs = fs.readdirSync(join(pathSave, 'videos', channel));
 
     if (!keepDay) {
@@ -82,6 +86,18 @@ function getSchedules(scheduleName) {
             var chns = [];
             if (arr.length > 0) {
                 for (var i = 0; i < arr.length; i++) {
+                    var sch = addSchedule(
+                        arr[i].id,
+                        arr[i].channel,
+                        arr[i].date,
+                        arr[i].fileName,
+                        arr[i].streamPath,
+                        arr[i].startDate,
+                        arr[i].endDate);
+                    schedules.push(arr[i].id);
+                }
+
+                for (var i = 0; i < arr.length; i++) {
                     var isValid = true;
                     for (var j = 0; j < chns.length; j++) {
                         if (chns[j] == arr[i].channel) {
@@ -93,15 +109,6 @@ function getSchedules(scheduleName) {
                         deleteFolderOld(arr[i].channel, arr[i].date, arr[i].keepDay);
                         chns.push(arr[i].channel);
                     }
-                    var sch = addSchedule(
-                        arr[i].id,
-                        arr[i].channel,
-                        arr[i].date,
-                        arr[i].fileName,
-                        arr[i].streamPath,
-                        arr[i].startDate,
-                        arr[i].endDate);
-                    schedules.push(arr[i].id);
                 }
             }
         });
@@ -117,7 +124,7 @@ function getSchedules(scheduleName) {
     // End the request
     request.end();
 }
-function addSchedule(id, channel, date, fileName, streamPath, startDateStr, endDateStr) {
+function addSchedule(id, channel, date, fileName, streamUrl, startDateStr, endDateStr) {
     //2019-01-01T00:00:00
     var exist = schedules.filter((e) => { return e == id; }).length > 0;
     //console.log(exist)
@@ -133,10 +140,18 @@ function addSchedule(id, channel, date, fileName, streamPath, startDateStr, endD
     var endDate = new Date(endDateStr);
     startDate = addMinutes(startDate, diffTime);
     endDate = addMinutes(endDate, diffTime);
+    //expired
+    if (endDate < new Date()) {
+        return;
+    }
 
+    //start lated
+    if (startDate < new Date()) {
+        startDate = new Date();
+    }
     //console.log("run: ", startDate);
     var videoName = fileName;
-    var timeMs = (endDate - startDate) / 1000;
+    var duration = (endDate - startDate) / 1000;
     var res = {
         id,
         channel,
@@ -159,21 +174,161 @@ function addSchedule(id, channel, date, fileName, streamPath, startDateStr, endD
     files.push(fileId);
     const job = schedule.scheduleJob(fileId, {
         start: startDate,
-        end: endDate,
         rule: '* * * * * *'
     }, function () {
-        downloadBasic(timeMs, dir, streamPath, videoName);
-        schedule.cancelJob(fileId);
+        try {
+            download({ dir, videoName, streamUrl, duration });
+        }
+        catch { }
+        schedule.cancelJob(job);
     }, function () {
         res.added = true;
-        console.log("[" + channel + "] add [" + videoName + "] to schedule successfully ");
+        console.log("[" + channel + "] ADD [" + videoName + "] SUCCESS");
     });
     return res;
 }
 
 function addMinutes(date, minutes) {
-    var d2 = new Date(date.getTime() + minutes*60000);
+    var d2 = new Date(date.getTime() + minutes * 60000);
     return d2;
+}
+
+function addSeconds(date, seconds) {
+    var d2 = new Date(date.getTime() + seconds * 1000);
+    return d2;
+}
+
+function toSeconds(hms) {
+    var a = hms.split(':');
+    var seconds = (+a[0]) * 60 * 60 + (+a[1]) * 60 + (+a[2]);
+    return seconds + 1;
+}
+
+function finish(rs) {
+    if (rs.isMultiFile == true) {
+        console.log("--append txt: ", JSON.stringify(rs));
+        fs.appendFileSync(rs.textPath, "file '" + rs.fileRootPath + "'\n")
+        for (var i = 0; i < rs.numFile; i++) {
+            var f = rs.fileRootPath.replace(".mp4", "_" + (i + 1) + ".mp4");
+            if (fs.existsSync(f)) {
+                fs.appendFileSync(rs.textPath, "file '" + f + "'\n");
+            }
+        }
+
+        var finalFile = rs.fileRootPath.replace(".mp4", "_final.mp4");
+        console.log("--merge files: ", finalFile);
+        var args = [
+            "-f", "concat",
+            "-safe", "0",
+            "-i", rs.textPath,
+            "-c", "copy",
+            finalFile
+        ]
+        var proc = spawn("ffmpeg", args);
+        proc.stderr.setEncoding("utf8")
+        proc.stdout.on('data', function (data) {
+        });
+        proc.stderr.on('data', function (data) {
+        });
+        proc.stderr.on('error', function (error) {
+            console.log(error);
+        });
+        proc.on('close', function (data) {
+            for (var i = 0; i < rs.numFile; i++) {
+                var f = rs.fileRootPath.replace(".mp4", "_" + (i + 1) + ".mp4");
+                if (fs.existsSync(f)) {
+                    //console.log(f);
+                    fs.rmSync(f);
+                }
+            }
+            if (fs.existsSync(rs.fileRootPath))
+                fs.rmSync(rs.fileRootPath);
+
+            if (fs.existsSync(rs.textPath))
+                fs.rmSync(rs.textPath);
+
+            if (fs.existsSync(finalFile))
+                fs.renameSync(finalFile, rs.fileRootPath);
+            console.log("--close merge: ", data);
+        });
+    }
+}
+
+function download({ dir, videoName, streamUrl, duration }) {
+    var num = 0;
+    var obj = { dir, videoName, streamUrl, duration, num, onFinish: finish };
+    console.log("--start download: ", JSON.stringify(obj));
+    execFfmpeg(obj);
+}
+
+function execFfmpeg({ dir, videoName, streamUrl, duration, num, onFinish }) {
+    var startTime = new Date();
+    var cmd = 'ffmpeg';
+    var seconds = 0;
+    // var root = dir + "/" + videoName;
+    // var newName = num == 0 ? root : root + "_" + num;
+    var videoRootPath = join(dir, videoName + '.mp4');
+    var videoPath = join(dir, num == 0 ? (videoName + '.mp4') : (videoName + "_" + num + '.mp4'));
+    var textPath = join(dir, videoName + '.txt');
+    var args = [
+        '-y',
+        '-i', streamUrl,
+        '-t', duration,
+        '-c', 'copy',
+        '-bsf:a', 'aac_adtstoasc',
+        videoPath
+    ];
+
+    console.log("--[" + videoRootPath + "] start now");
+    var proc = spawn(cmd, args);
+    proc.stdout.on('data', function (data) {
+    });
+    proc.stderr.setEncoding("utf8");
+    proc.stderr.on('data', function (data) {
+        //console.log("process record: ", data);
+        if (data.indexOf("time=") > -1) {
+            var i = data.indexOf("time=") + "time=".length;
+            var t = data.substring(i, i + 8);
+            seconds = toSeconds(t);
+            //console.log(videoRootPath + " time: ", t);
+        }
+    });
+    proc.stderr.on('error', function (error) {
+        console.log("stderr error: ", error);
+    });
+    proc.on('close', function (data) {
+        //console.log("close record: ", data);
+        console.log("--[" + videoRootPath + "] seconds, duration: ", seconds, duration);
+        if (seconds < duration) {
+            var newDuration = duration - seconds;
+            console.log("--[" + videoRootPath + "] restart remaining: ", newDuration);
+            if (num < maxRetry) {
+                num++;
+                setTimeout(function () {
+                    var newObj = {
+                        startTime,
+                        duration: newDuration,
+                        dir,
+                        videoName,
+                        streamUrl,
+                        num,
+                        onFinish: finish
+                    };
+                    execFfmpeg(newObj);
+                }, 3000);
+            }
+        }
+        else {
+            if (typeof (onFinish) == 'function') {
+                onFinish({
+                    isMultiFile: num > 0,
+                    textPath: textPath,
+                    fileRootPath: videoRootPath,
+                    numFile: num
+                });
+            }
+        }
+    });
 }
 
 function downloadBasic(time, dir, streamPath, videoName) {
@@ -195,7 +350,7 @@ function downloadBasic(time, dir, streamPath, videoName) {
 
     proc.stdout.on('data', function (data) {
         if (!downloaderRunning) {
-            console.log("dowwnload [" + name + "] running");
+            console.log("[" + name + "] running");
             downloaderRunning = true;
         }
     });
@@ -203,17 +358,16 @@ function downloadBasic(time, dir, streamPath, videoName) {
     proc.stderr.setEncoding("utf8")
     proc.stderr.on('data', function (data) {
         if (!downloaderRunning) {
-            console.log("download [" + name + "] running");
+            console.log("[" + name + "] running");
             downloaderRunning = true;
         }
     });
 
     proc.on('close', function () {
-        console.log("download [" + name + "] successfully");
+        console.log("[" + name + "] SUCCESS");
         schedules = schedules.filter(m => m.added == false && m.videoName != videoName);
     });
 }
-
 
 if (process.env.START_JOB_GET_SCHEDULE.length > 0) {
     const rule = new schedule.RecurrenceRule();
@@ -229,4 +383,4 @@ if (process.env.START_JOB_GET_SCHEDULE.length > 0) {
     });
 }
 
-module.exports = { downloadBasic, getSchedules, deleteFolderOld, addSchedule, schedules };
+module.exports = { downloadBasic, getSchedules, deleteFolderOld, addSchedule, finish, download, execFfmpeg, addSeconds, addMinutes, toSeconds, schedules };
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ console.log("VIDEO_KEEP_DAY=", process.env.VIDEO_KEEP_DAY);
 console.log("PATH_SAVE=", process.env.PATH_SAVE);
 var schedules = [];
 var currentDateJob = "";
-console.log(sch);
+// console.log(sch);
 
 var pathSave = process.env.PATH_SAVE;
 var id = 0;
@@ -36,61 +36,6 @@ app.get('/', (request, response) => {
     response.send('Hello, Server!');
 });
 
-// app.get('/schedule', (request, response) => {
-//     console.log(`URL: ${request.url}`);
-//     response.setHeader('Content-Type', 'application/json');
-//     response.end(JSON.stringify(schedules));
-// });
-
-// app.post('/schedule/add', (request, response) => {
-//     console.log(`URL: ${request.url}`);
-//     console.log(request.body);
-//     var sch2 = sch.addSchedule(
-//         request.body.id,
-//         request.body.channel,
-//         request.body.date,
-//         request.body.fileName,
-//         request.body.streamPath,
-//         request.body.startDate,
-//         request.body.endDate);
-//     if (sch2 != null) {
-//         schedules.push(request.body.id);
-//         response.send(sch?.videoName);
-//     }
-//     else {
-//         console.log("existed");
-//         response.status(422).send("schedule existed");
-//     }
-// });
-
-// app.post('/schedule/addMore', bodyParser, (request, response) => {
-//     console.log(`URL: ${request.url}`);
-//     if (Array.isArray(request.body)) {
-//         var messages = [];
-//         for (var i = 0; i < request.body.length; i++) {
-//             var b = request.body[i];
-//             var sch = sch.addSchedule(
-//                 b.id,
-//                 b.channel,
-//                 b.date,
-//                 b.fileName,
-//                 b.streamPath,
-//                 b.startDate,
-//                 b.endDate);
-//             if (sch != null) {
-//                 schedules.push(b.id);
-//                 messages.push(sch?.videoName);
-//             }
-//             else {
-//                 messages.push("schedule existed");
-//             }
-//         }
-//         response.send("success " + request.body.length);
-//     }
-//     else {
-//         response.status(500).send("body is not a array");
-//     }
-// });
 
 app.post('/schedule/start', bodyParser.json(), (request, response) => {
     console.log(`URL: ${request.url}`);
